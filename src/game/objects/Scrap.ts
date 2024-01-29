2180f9c12a4789e8d6436dc23389132be8919fa1
Alec Vickers <alecvickers@hotmail.com>
2024-01-29 20:27:18 +0000

    Added slight spin to scrap

--- src/game/objects/Scrap.ts
+++ src/game/objects/Scrap.ts
@@ -11,5 +11,9 @@ export class Scrap extends GameObject {
         this.setBody(body);
 
         this.body.frictionAir = 0.001;
+
+        const randomAngular = Math.random() > 0.5 ? 0.01 : -0.01;
+
+        Body.setAngularVelocity(this.body, this.body.angularSpeed + randomAngular);
     }
 }
